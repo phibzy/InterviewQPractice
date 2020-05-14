d263cd40c828fb557308a46c2c66d479c026fa69
phibzy <phibzy@gmail.com>
2020-05-14 15:36:02 +1000

    Think I have solution, need to handle a few edge cases

---
+++ CourseSchedule/courseSchedule.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+"""
+Takes in numCourses(int) and prerequisites (list of list of int) aka graph edges
+
+Returns if possible to finish all courses
+
+Questions to Interviewer:
+    - Will there be redundant prereqs? E.g. [[1,0], [2,1], [2,0]]
+    - Multiple prereqs?
+    - Max numCourses?
+    - Range of prereqs size?
+
+Algo:
+     If prereqs >= numCourses then impossible to finish all courses
+     Make directed graph (adj matrix) of prereqs
+     Hash of coords to check if we've visited coord before
+     Do DFS/BFS on graph, if you end up at node you already visited, then it's impossible to finish all courses
+     Logic: if that happens, then you need to finish course further down the line to do course you've already completed,
+            which makes no nense
+
+
+"""
+
+class Solution:
+
+    def canFinish(self, numCourses, prerequisites):
+        if prerequisites == []:              return True
+        # if len(prerequisites) >= numCourses: return False
+
+        # Step 1 - make da graph
+        graph = [[] for _ in range(numCourses)]
+        for i in range(numCourses):
+            graph[i] = [0 for _ in range(numCourses)]
+
+
+        # Step 2 - Add edges
+        for x, y in prerequisites:
+            if not (0 <= x < numCourses and 0 <= y < numCourses): return False
+            graph[y][x] = 1
+        
+        # print(graph)
+
+        # Step 3 - do search
+        doneDFS = dict()
+        for x in range(numCourses):
+            if x in doneDFS: continue
+            visited = dict()
+            # If returns false, then we break/return false
+            if not self.dfs(x, numCourses, graph, visited, doneDFS):
+                return False
+
+        return True
+
+
+    def dfs(self, x, numCourses, graph, visited, doneDFS):
+        # Problem with algo - check if it visits origin? But that's wrong since you could get cycle elsewhere
+        if x in visited: return False
+        if x in doneDFS: return True
+        visited[x] = True
+        doneDFS[x] = True
+
+        for i in range(numCourses):
+            if graph[x][i] == 1:
+                if not self.dfs(i, numCourses, graph, visited, doneDFS):
+                    return False
+
+        return True
+
+
+
---
+++ CourseSchedule/test.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""
+Cases:
+    - No prereqs
+    - More or equal prereqs to courses
+    - Prereq of itself
+    - Out of bounds course
+    - Normal long prereq list
+    - Two paths to course on prereq tree
+    - Isolated course
+    - One course, at least one prereq
+
+"""
+from courseSchedule import Solution
+import unittest
+
+class testCourseSchedule(unittest.TestCase):
+
+    a = Solution()
+
+    def testNoPrereq(self):
+        self.assertTrue(self.a.canFinish(2, []), "Error: Fails 0 prereq test")
+
+    def testMorePrereqThanCourse(self):
+        self.assertTrue(self.a.canFinish(4, [[1,0],[2,1],[3,2],[3,0]]), "Error: Fails redundant prereq case")
+
+    def testOutOfBoundsCourse(self):
+        self.assertFalse(self.a.canFinish(4, [[1,0],[2,4],[5,2],[3,-1]]), "Error: Fails out of bounds course case")
+
+    def testPrereqOfItself(self):
+        self.assertFalse(self.a.canFinish(5, [[1,0],[2,4],[2,2],[5,3]]), "Error: Fails prereq of itself case")
+
+    def testTwoPathsToOneCourse(self):
+        self.assertTrue(self.a.canFinish(5, [[1,0],[2,1],[3,1],[4,2], [4,3]]), "Error: Fails two path case")
+
+    def testNormalLongPrereqCourse(self):
+        self.assertTrue(self.a.canFinish(9, [[1,0],[2,1],[3,2],[4,2],[5,3],[6,3],[7,4],[8,7]]), "Error - fails long prereq course case")
+
+    def testOneCoursePrereqItself(self):
+        self.assertFalse(self.a.canFinish(1, [[0,0]]), "Error: Fails prereq of itself case (1 course)") 
+
+    def testOneCourseAtLeastOnePrereq(self):
+        self.assertFalse(self.a.canFinish(1, [[1,0]]), "Error: Fails prereq for 1 course case") 
+
+    def testIsolatedCourses(self):
+        self.assertTrue(self.a.canFinish(12, [[1,0],[2,1],[3,2],[4,2],[5,3],[6,3],[7,4],[8,7]]), "Error - fails isolated courses case")
+
+
