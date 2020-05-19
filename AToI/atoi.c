3db98863ffcc94125455c135682f49b9a09784cd
phibzy <phibzy@gmail.com>
2020-05-19 18:29:41 +1000

    Cheesed it with a long, sorry not sorry

--- AToI/atoi.c
+++ AToI/atoi.c
@@ -1,21 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <limits.h>
 
 int myAtoi(char * s);
-int numSum(char * s, int count);
+int numSum(char * s, int count, int negSign);
 
 
 int main(int argc, char*argv[]) {
   
    int thing = myAtoi("-91283472332");
+   /*int thing = myAtoi("-912");*/
    printf("%d", thing);
 
    return EXIT_SUCCESS;
 }
 
 int myAtoi(char * s) {
-   unsigned int total = 0;
+   long total = 0;
    int negSign = 1;
    int count = 0;
 
@@ -28,10 +30,10 @@ int myAtoi(char * s) {
          if (nextChar == '+' || nextChar == '-') {
             if (nextChar == '-') { negSign = -1; }
             count++;
-            total = numSum(s, count);
+            total = numSum(s, count, negSign);
             break;
          } else if ('0' <= nextChar && nextChar <= '9') {
-            total = numSum(s, count);
+            total = numSum(s, count, negSign);
             break;
          } else {
             break;
@@ -42,36 +44,29 @@ int myAtoi(char * s) {
       nextChar = s[count];
    }   
 
-   total = total * negSign;
-
-   if (total < pow(-2,31)) {
-
-      total = pow(2, 31); 
-
-   } else if (total > pow(2, 31) - 1) {
-  
-      total = pow(2, 31) - 1;
-
-   }
-
-   int rTotal = 0;
-
-   rTotal = total * negSign;
-
-   return rTotal;
+   return total;
 }
 
 
-int numSum(char * s, int count) {
-      unsigned int sum = 0;
+int numSum(char * s, int count, int negSign) {
+      long sum = 0;
       char nextChar = s[count];
       int val = 0;
 
       while ('0' <= nextChar && nextChar <= '9') {
-         printf("nextChar is %c\n", nextChar);
          val = nextChar - '0';
-         sum = 10*sum + val;
-         printf("sum is %d\n", sum);
+
+         sum = 10*sum + val*negSign;
+         
+         if (sum <= INT_MIN) {
+            sum = INT_MIN;
+            break;
+         }
+
+         if (sum >= INT_MAX) {
+            sum = INT_MAX;
+            break;
+         }
 
          count++;
          nextChar = s[count];
