e62f338e67057ddf33e11f3ef5e38a15ca4ed5ef
phibzy <phibzy@gmail.com>
2020-05-25 15:51:21 +1000

    Got it out, should probably do again but neater

--- ReverseNodesKGroup/reverseKGroup.py
+++ ReverseNodesKGroup/reverseKGroup.py
@@ -15,6 +15,8 @@ logging.basicConfig(level=logging.DEBUG, format="%(levelname)s - %(msg)s")
 
 class Solution:
     def reverseKGroup(self, head, k):
+        # pdb.set_trace()
+
         if k == 0 or k == 1 or head is None: return head
         
         # will need to change head
@@ -22,21 +24,22 @@ class Solution:
         end = start 
     
         while end:
-            logging.debug(''.rjust(30, '-'))
-            logging.debug('NEW MAIN LOOP')
-            logging.debug(''.rjust(30, '-'))
+            nextStart = end
+            # logging.debug(''.rjust(30, '-'))
+            # logging.debug('NEW MAIN LOOP')
+            # logging.debug(''.rjust(30, '-'))
 
-            logging.debug(f"start is {start.val}")
-            logging.debug(f"end is {end.val}")
+            # logging.debug(f"start is {start.val}")
+            # logging.debug(f"end is {end.val}")
 
             kCount = k
 
-            logging.debug(f"entering kcount loop")
+            # logging.debug(f"entering kcount loop")
             while kCount > 1 and end.next:
                 kCount -= 1
                 end = end.next
 
-            logging.debug(f"end is {end.val}")
+            # logging.debug(f"end is {end.val}")
             
 
 
@@ -46,19 +49,21 @@ class Solution:
             rest = end.next
             end = start
 
-            logging.debug(f"end is {end.val}")
+            if end != head: start = start.next
+
+            # logging.debug(f"end is {end.val}")
             
-            if rest:
-                logging.debug(f"rest is {rest.val}")
-            else:
-                logging.debug("rest is None")
+            # if rest:
+                # logging.debug(f"rest is {rest.val}")
+            # else:
+                # logging.debug("rest is None")
 
             nextNode = start.next
             start.next = rest
 
             while nextNode != rest:
-                logging.debug(f"nextNode is {nextNode.val}")
-                logging.debug(f"start is {start.val}")
+                # logging.debug(f"nextNode is {nextNode.val}")
+                # logging.debug(f"start is {start.val}")
 
                 curr = nextNode.next
                 nextNode.next = start
@@ -68,12 +73,15 @@ class Solution:
             if end == head:
 
                 head = start
+                start = end
 
             else:
-                end.next = start    
 
-            start = end
-            end = end.next
+                end.next = start    
+                start = nextStart
+
+            end = start.next
+            # pdb.set_trace()
         
         return head
 
@@ -84,5 +92,6 @@ def printList(head):
         
 a = Solution()
 
-thing = a.reverseKGroup(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))), 2)
+thing = a.reverseKGroup(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))), 5)
+printList(thing)
 
