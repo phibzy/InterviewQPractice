2d538359283fb17aee9bf3688f6a8586c51b9b5d
phibzy <phibzy@gmail.com>
2020-05-25 13:53:07 +1000

    Started reverseKGroup, bug doing my head in

---
+++ ReverseNodesKGroup/reverseKGroup.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+
+#Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+import logging
+import pdb
+
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)s - %(msg)s")
+# logging.disable(logging.DEBUG)
+
+class Solution:
+    def reverseKGroup(self, head, k):
+        if k == 0 or k == 1 or head is None: return head
+        
+        # will need to change head
+        start = head
+        end = start 
+    
+        while end:
+            logging.debug(''.rjust(30, '-'))
+            logging.debug('NEW MAIN LOOP')
+            logging.debug(''.rjust(30, '-'))
+
+            logging.debug(f"start is {start.val}")
+            logging.debug(f"end is {end.val}")
+
+            kCount = k
+
+            logging.debug(f"entering kcount loop")
+            while kCount > 1 and end.next:
+                kCount -= 1
+                end = end.next
+
+            logging.debug(f"end is {end.val}")
+            
+
+
+            # not a multiple, no need to do anything more
+            if kCount > 1: return head
+
+            rest = end.next
+            end = start
+
+            logging.debug(f"end is {end.val}")
+            
+            if rest:
+                logging.debug(f"rest is {rest.val}")
+            else:
+                logging.debug("rest is None")
+
+            nextNode = start.next
+            start.next = rest
+
+            while nextNode != rest:
+                logging.debug(f"nextNode is {nextNode.val}")
+                logging.debug(f"start is {start.val}")
+
+                curr = nextNode.next
+                nextNode.next = start
+                start = nextNode
+                nextNode = curr
+
+            if end == head:
+
+                head = start
+
+            else:
+                end.next = start    
+
+            start = end
+            end = end.next
+        
+        return head
+
+def printList(head):
+    while head:
+        print(f" --> {head.val}") 
+        head = head.next
+        
+a = Solution()
+
+thing = a.reverseKGroup(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))), 2)
+
