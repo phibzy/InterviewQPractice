1500b32f7477f416832b4aafd3e8fb6c602d6789
phibzy <phibzy@gmail.com>
2020-05-27 16:40:31 +1000

    Finished proper implementation with index return

--- Solutions/BinarySearch/binarySearch.py
+++ Solutions/BinarySearch/binarySearch.py
@@ -6,23 +6,24 @@ Implementation of binary search
 """
 
 def binarySearch(items, target):
-    if not items: return False
+    if not items: return -1
 
-    endI = len(items) 
+    endI = len(items) - 1
     middle = len(items) // 2
     startI = 0
 
-    if items[0] > target or items[-1] < target: return False
+    if items[0] > target or items[-1] < target: return -1
 
-    while startI != middle:
-        if items[middle] == target: return True
+    while endI >= startI:
+        if items[middle] == target: return middle
 
         if items[middle] > target:
-            endI = middle
+            endI = middle - 1
         else:
-            startI = middle
+            startI = middle + 1
 
         middle = (startI + endI + 1) // 2
 
         
-    return items[middle] == target
+    return -1 
+
--- Solutions/BinarySearch/test.py
+++ Solutions/BinarySearch/test.py
@@ -19,25 +19,25 @@ from binarySearch import binarySearch
 class testBinarySearch(unittest.TestCase):
 
     def testEmptyList(self):
-        self.assertFalse(binarySearch([], 4))
+        self.assertTrue(binarySearch([], 4) == -1)
 
     def testLength1Pass(self):
-        self.assertTrue(binarySearch([4], 4))
+        self.assertTrue(binarySearch([4], 4) == 0)
 
     def testLength1Fail(self):
-        self.assertFalse(binarySearch([-200], 4))
+        self.assertTrue(binarySearch([-200], 4) == -1)
 
     def testOddLengthList(self):
-        self.assertTrue(binarySearch([2,7,10,12,18], 7))
+        self.assertTrue(binarySearch([2,7,10,12,18], 7) == 1)
 
     def testEvenLengthList(self):
-        self.assertTrue(binarySearch([2,7,10,12,18,2000], 18))
+        self.assertTrue(binarySearch([2,7,10,12,18,2000], 18) == 4)
 
     def testFirstPassSuccess(self):
-        self.assertTrue(binarySearch([-2,-1,0,1,2], 0))
+        self.assertTrue(binarySearch([-2,-1,0,1,2], 0) == 2)
 
     def testLastPassSuccess(self):
-        self.assertTrue(binarySearch([-2,-1,0,1,2,200,420,3700,9999,10000,12000],12000))
+        self.assertTrue(binarySearch([-2,-1,0,1,2,200,420,3700,9999,10000,12000],12000) == 10)
 
     def testItemNotInListLong(self):
-        self.assertFalse(binarySearch([-30,-15,-5,-2,0,1,4,20,45,67,72,81,420,960],-3000))
+        self.assertTrue(binarySearch([-30,-15,-5,-2,0,1,4,20,45,67,72,81,420,960],-3000) == -1)
