05fae4e7b300e09ea1c98a1f5e3d03fb4e1459ea
phibzy <phibzy@gmail.com>
2020-05-29 20:10:44 +1000

    Approach 2 works now

--- Solutions/MergeTwoLists/mergeTwoLists.py
+++ Solutions/MergeTwoLists/mergeTwoLists.py
@@ -20,41 +20,73 @@ class Solution:
     # 1 - Create dummy head and just keep adding on 
     # 2 - Use one of the existing heads
 
+    # Approach 1
+    # def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # newL = None
+        # if l1 is None and l2 is None: return None
+        # if l1 and not l2: return l1
+        # if l2 and not l1: return l2
 
+
+        # if l1.val < l2.val:
+            # head = l1
+            # newL = l1
+            # l1 = l1.next
+
+        # else:
+            # head = l2
+            # newL = l2
+            # l2 = l2.next
+
+        # newL.next = None
+
+        # while l1 and l2:
+            # if l1.val < l2.val:
+                # newL.next = l1
+                # l1 = l1.next
+            # else:
+                # newL.next = l2
+                # l2 = l2.next
+            
+            # newL = newL.next
+            # newL.next = None
+        
+        # if l1: newL.next = l1                  
+        # if l2: newL.next = l2
+
+        # return head
+
+    # Approach 2
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        newL = None
-        if l1 is None and l2 is None: return None
+        if not l1 and not l2: return None
+        if not l1 and l2: return l2
         if l1 and not l2: return l1
-        if l2 and not l1: return l2
 
+        if l2.val < l1.val:
+            temp = l2
+            l2 = l1
+            l1 = temp
 
-        if l1.val < l2.val:
-            head = l1
-            newL = l1
+        head = l1
+
+        while l1.next and l2:
+            if l2.val < l1.next.val:
+                rest1 = l1.next
+                rest2 = l2.next
+                l1.next = l2
+                l1.next.next = rest1
+                l2 = rest2
+
             l1 = l1.next
 
-        else:
-            head = l2
-            newL = l2
-            l2 = l2.next
+        if l2: l1.next = l2
 
-        newL.next = None
+        return head        
 
-        while l1 and l2:
-            if l1.val < l2.val:
-                newL.next = l1
-                l1 = l1.next
-            else:
-                newL.next = l2
-                l2 = l2.next
-            
-            newL = newL.next
-            newL.next = None
         
-        if l1: newL.next = l1                  
-        if l2: newL.next = l2
 
-        return head
+
+
 
         
 
