d67865d7b1c1d8eca1d0c6b58734a5cf22551294
phibzy <phibzy@gmail.com>
2020-06-10 17:13:52 +1000

    Works, used another solution for inspiration.

--- Solutions/MinimumBribes/minBribes.py
+++ Solutions/MinimumBribes/minBribes.py
@@ -17,11 +17,20 @@ Minimum size of input? - E.g. will empty list be an option
 """
 
 def minimumBribes(q):
+    q = [ i-1 for i in q]
     count = 0
+
     for i, val in enumerate(q):
-        if (i - val + 1) > 2:
+        # No one can move more than 2 places forward, since can only bribe twice
+        if (val - i) > 2:
             print("Too chaotic")
-        
-        count += (i - val + 1)
+            return
+
+        for j in range(max(0, val-1), i):
+            if q[j] > val: count += 1
+
 
     print(count)
+
+# Which numbers are missing from in front of a given index?
+
