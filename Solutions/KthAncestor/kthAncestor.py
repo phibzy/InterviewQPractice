26d1fd772fee2355feaa54830664fcd03e1a0a05
phibzy <phibzy@gmail.com>
2020-07-03 12:37:48 +1000

    Working through other user's solution

--- Solutions/KthAncestor/kthAncestor.py
+++ Solutions/KthAncestor/kthAncestor.py
@@ -21,42 +21,90 @@ The k-th ancestor of a tree node is the k-th node in the path from that node to 
 # Time Complexity: O(N^2)?
 # Space Complexity: O(NlogN) - N nodes in hash, with at most logN elements in their list
 #                            - You will have at most the height of the tree in each list
+# class TreeAncestor:
+
+    # def __init__(self, n, parent):
+        # self.known  = dict()
+        # self.parent = parent
+        # self.size = n
+        
+        # i = self.size - 1
+        
+        # while i > 0:
+            # children = list()
+            # if i not in self.known:
+                # self.known[i] = list()
+                # self.populate(i, 0, children)
+                
+            # i -= 1
+              
+    # def populate(self, node, level, children):
+        # nextNode = self.parent[node]
+        # children.append(node)
+        
+        # for child in children:
+            # self.known[child].append(nextNode)
+            # if nextNode in self.known:
+                # self.known[child] += self.known[nextNode]    
+        
+        # if (nextNode not in self.known) and nextNode != 0:
+            # self.known[nextNode] = list()
+            # self.populate(nextNode, level + 1, children)
+            
+            
+        
+    # def getKthAncestor(self, node, k):
+        # if node == 0: return -1
+        # if k > len(self.known[node]): return -1
+        # k = k-1
+        
+        # # Use array, so change this to k-1 once it's going dawg
+        # return self.known[node][k]
+
+"""
+
+Based off user ye15's solution
+
+Time: O(NlogN). Preprocessing is NlogN and then lookup is logN
+Space: O(NlogN) - N * logN sized DP matrix 
+
+
+"""
+
+
+
+
+import math
 class TreeAncestor:
 
     def __init__(self, n, parent):
-        self.known  = dict()
-        self.parent = parent
-        self.size = n
+        maxA = int(math.log2(n)) + 1   
+        self.known = [ [-1]*maxA for _ in range(n) ] # Ancestor will be at most logN, aka height of tree
         
-        i = self.size - 1
-        
-        while i > 0:
-            children = list()
-            if i not in self.known:
-                self.known[i] = list()
-                self.populate(i, 0, children)
+        for j in range(maxA):
+            for i in range(n):
+                if j == 0: # First pass we calculate every node's first parent
+                    self.known[i][j] = parent[i]
                 
-            i -= 1
-              
-    def populate(self, node, level, children):
-        nextNode = self.parent[node]
-        children.append(node)
-        
-        for child in children:
-            self.known[child].append(nextNode)
-            if nextNode in self.known:
-                self.known[child] += self.known[nextNode]    
-        
-        if (nextNode not in self.known) and nextNode != 0:
-            self.known[nextNode] = list()
-            self.populate(nextNode, level + 1, children)
-            
-            
+                # Before computing parents of parents, check that a parent exists
+                elif self.known[i][j-1] != -1:
+                    
+                    # My jth parent  is my (j-1)th parent's (j-1)th parent
+                    self.known[i][j] = self.known[self.known[i][j-1]][j-1]     
         
     def getKthAncestor(self, node, k):
-        if node == 0: return -1
-        if k > len(self.known[node]): return -1
-        k = k-1
         
-        # Use array, so change this to k-1 once it's going dawg
-        return self.known[node][k]
+        """
+        Every node has logN entries in known matrix
+        
+        
+        """
+        
+        #### NEED TO GO OVER THIS PART, EVERYTHING ELSE MAKES SENSE ####
+        
+        while k > 0 and node != -1: 
+            i = int(math.log2(k))
+            node = self.known[node][i]
+            k -= (1 << i)
+
+        return node 
