622cdfd297d214b8aa06b51b8a9eb853631c6377
phibzy <phibzy@gmail.com>
2021-02-26 12:49:07 +1100

    Iterative solution also done

--- Solutions/ScoreOfParentheses/parentheses.py
+++ Solutions/ScoreOfParentheses/parentheses.py
@@ -19,8 +19,37 @@ SC: O(N) - We have at most N/2 recursive calls
 
 """
 class Solution:
-    def scoreOfParentheses2(self, s):
-        pass
+    def scoreOfParentheses(self, s):
+        # Use two stacks, one for adding and one for subtracting
+        mult = list()
+        add = list()
+
+        total = 0
+        i = 0
+    
+        # Very similar comparisons to before
+        while i < len(s):
+
+            # If there's a closing bracket, we've been in a nested
+            # parentheses expression
+            if s[i] == ")":
+                total *= 2
+                total += mult.pop()
+
+            # Check for starting nested case
+            else:
+                if s[i+1] == ")":
+                    total += 1
+                    i += 1
+
+                # Otherwise we're starting a nested case
+                else:
+                    mult.append(total)
+                    total = 0
+
+            i += 1
+
+        return total
 
     # OG Recursive solution
     def scoreOfParentheses2(self, s):
