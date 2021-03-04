d2975d72ef644c8e814409e332b03ee4f13f9158
phibzy <phibzy@gmail.com>
2021-03-04 13:16:35 +1100

    Constant space solution done

--- Solutions/SetMismatch/setMismatch.py
+++ Solutions/SetMismatch/setMismatch.py
@@ -58,11 +58,35 @@ class Solution:
         return [duplicate, (realSum - (setSum - duplicate))]
 
     # Set version
-    def findErrorNums(self, nums):
+    def findErrorNums2(self, nums):
         realSet = set(range(1, len(nums) + 1))
         numSet = set(nums)
         missingNo = list(realSet - numSet)[0]
 
         return sorted([missingNo, missingNo + sum(nums) - sum(realSet)])
 
+    # Going for constant space    
+    def findErrorNums(self, nums):
+        # Mark values by making them negative (since values can't be less than 0)
+        # Do one pass, every time you see a value, mark that index
+        # If already marked, we've found the duplicate
+        # Keep going, only index unmarked will be the missing number
+        # Do another pass to find it
         
+        # Have to take abs values since some can be marked already
+        for i in range(len(nums)):
+            # Use -1 because 1 indexing
+            checkIndex = abs(nums[i]) - 1
+
+            if nums[checkIndex] < 0:
+                duplicate = abs(nums[i])
+
+            # Mark it if unmarked
+            else:
+                nums[checkIndex] *= -1
+
+        # Return duplicate and index of only remaining positive number in nums
+        return [duplicate] + [i+1 for i,v in enumerate(nums) if v > 0]
+
+
+
