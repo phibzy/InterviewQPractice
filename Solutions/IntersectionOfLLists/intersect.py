0dedca5bd8d29cb36534338dc362f14dddf9273f
phibzy <phibzy@gmail.com>
2021-03-08 12:32:52 +1100

    Did non-modifying solution

--- Solutions/IntersectionOfLLists/intersect.py
+++ Solutions/IntersectionOfLLists/intersect.py
@@ -75,3 +75,43 @@ class Solution:
 
         return intersectVal
 
+"""
+Since problem description contradicts engine results:
+    O(1) space and O(N^2) time Solution:
+        For every node in A, check every node in B to see if it's the same
+        Optimisations: Find shorter list to speed up checks
+
+    O(1) Space and O(N) time:
+        If intersection:
+            len(a) +len(c) + len(b) = len(b) + len(c) + len(a)
+            where c is the common "tail" of each list
+
+            because of this, we can loop through a and b, checking if they're equal.
+            if a reaches null, set it to head of b
+            if b reaches null, set it to head of a
+
+            This will work since after resetting pointers, since they'll differ by
+            difference of len(a) and len(b), meaning that the pointer switch will put them in sync
+
+
+"""
+class Solution2:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        a, b = headA, headB
+        
+        if not headA or not headB: return None
+        
+        # Even if they equal None at the end, we can reset them
+        while headA != headB:
+            if not headA: 
+                headA = b
+            else:
+                headA = headA.next
+                
+            if not headB:
+                headB = a
+            else:
+                headB = headB.next
+            
+        return headA
+
