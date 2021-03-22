c4d3a32ffb9b08995ef7cc97abb6e70c25843413
phibzy <phibzy@gmail.com>
2021-03-22 12:22:55 +1100

    Accepted solution, will try funky solution later

--- Solutions/SwappingNodesInLinkedList/swapNodes.py
+++ Solutions/SwappingNodesInLinkedList/swapNodes.py
@@ -26,6 +26,16 @@ class ListNode:
 
         return self.val == x.val and self.next == x.next
 
+    def __str__(self):
+        output = "--> "
+        curr = self
+
+        while curr:
+            output += f"{curr.val} --> "
+            curr = curr.next
+
+        return output
+
 class Solution:
     def swapNodes(self, head: ListNode, k: int) -> ListNode:
         ogHead = head
@@ -34,17 +44,19 @@ class Solution:
         length = 2
 
         # Need condition for new head
-        if k != 1:
-            # First pass, get pointer to node before target and length of list
-            while curr1.next != None:
-                if length == k:
-                    head = curr1
+        # First pass, get pointer to node before target and length of list
+        while curr1.next != None:
+            if length == k:
+                head = curr1
 
-                curr1 = curr1.next
-                length += 1
+            curr1 = curr1.next
+            length += 1
         
-        curr1 = head
+        curr1 = ogHead if k == 1 else head
         head = ogHead
+
+        # If they're the same node we don't need to change list
+        if k == length - k: return ogHead
 
         # Find index of swap node based on length
         if length - k != 1:
@@ -54,29 +66,46 @@ class Solution:
             while c != target:
                 curr2 = curr2.next
                 c += 1
-        
-        # If one of the swapped nodes is the head,
-        # we have to treat it differently
-        if k == 1 or length - k == 1: 
-            # Swap pointers if curr2 is the head
-            if length - k < k: curr2 = curr1
 
-            temp = curr2.next
-            curr2.next = head
-            temp.next = head.next
+        # Swap pointers to preserve curr1/curr2 order
+        if length - k < k: 
+            curr2, curr1 = curr1, curr2
+
+        if k == 1 or length - k == 1:
+            curr1.val, curr2.next.val = curr2.next.val, curr1.val
+
+        else:
+            curr1.next.val, curr2.next.val = curr2.next.val, curr1.next.val
+
+        return ogHead
+
+        # # Swap pointers to preserve curr1/curr2 order
+        # if length - k < k: 
+            # curr2, curr1 = curr1, curr2
+
+        # # If one of the swapped nodes is the head,
+        # # we have to treat it differently
+        # if k == 1 or length - k == 1: 
+
+            # temp = curr2.next
+            # curr2.next = head
+            # temp.next = head.next
+            # head = temp 
+
+        # else:
+            # # Need to preserve tails too
+            # temp = curr2.next
+            # tempTail = curr2.next.next
+
             
-            head = curr2
+            # curr1Tail = curr1.next.next
+            # curr2.next = curr1.next
+            # curr1.next.next = tempTail
 
             
 
 
-
             
 
 
-
-
-
-
-
-        
+            
