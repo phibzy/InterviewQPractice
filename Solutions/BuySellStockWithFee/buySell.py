81e5bd79daae0a093a843f130eb4dd97d75f3bf5
phibzy <phibzy@gmail.com>
2021-03-29 12:36:22 +1100

    Wrote some tests, will try tomorrow

---
+++ Solutions/BuySellStockWithFee/buySell.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+  @author      : Chris Phibbs
+  @created     : Monday Mar 29, 2021 12:11:58 AEDT
+  @file        : buySell
+
+"""
+
+"""
+Qs:
+    - Range of elements in list? Only positive numbers
+    - Range of fee? 0 or higher
+    - Number of elements? 1 or greater
+    - What do we return in case of no profit to be made? 0
+    - Can buy in at multiple different prices while already holding? No, must sell before buy
+
+"""
+
+class Solution:
+    def maxProfit(self, prices, fee: int) -> int:
+        pass
+  
---
+++ Solutions/BuySellStockWithFee/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+    Test Cases:
+        - Length 1 list
+        - Length 2 list
+        - Fee large enough to destroy any hope of profit
+        - Obvious, only 1 buy required case
+        - Multiple buy cases:
+            - 0 fee
+            - >0 fee, where greedy approach wouldn't work
+
+"""
+
+import unittest
+from buySell import Solution
+
+class test(unittest.TestCase):
+
+    a = Solution()
+
+    def testBasic(self):
+        self.assertEqual(self.a.maxProfit([1], 300), 0)
+        self.assertEqual(self.a.maxProfit([9,8,7,6,5,4,3,2,1], 0), 0)
+        self.assertEqual(self.a.maxProfit([1,9], 3), 5)
+        self.assertEqual(self.a.maxProfit([1, 9, 13, 90, 32, 5, 16, 88, 153], 152), 0)
+        self.assertEqual(self.a.maxProfit([1, 3, 2, 9, 1, 4, 4, 1, 6], 3), 8)
+
+    def testMulti(self):
+        self.assertEqual(self.a.maxProfit([2,9,5,8,16,5,17], 2), 24)
+
