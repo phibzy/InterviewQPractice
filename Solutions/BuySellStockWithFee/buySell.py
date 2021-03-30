0c8641ffb48c862ebb4e5471ae0915780b0a9f98
phibzy <phibzy@gmail.com>
2021-03-31 10:54:13 +1100

    Passed own tests, fails online. Will troubleshoot later

--- Solutions/BuySellStockWithFee/buySell.py
+++ Solutions/BuySellStockWithFee/buySell.py
@@ -17,7 +17,71 @@ Qs:
 
 """
 
+"""
+Algo:
+    - Start at first number which isn't followed by a lower number
+    - Make a queue, store starting number and 0 as tuple 
+
+"""
+
+from collections import deque
+
 class Solution:
     def maxProfit(self, prices, fee: int) -> int:
-        pass
-  
+        q = deque()
+
+        # Find first number that's not followed by lower number
+        lastNum = prices[0]
+
+        i = 1
+        while i < len(prices) and prices[i] < lastNum:
+            lastNum = prices[i]
+            i += 1
+
+        # Add first number to queue, then we starteth
+        q.append([lastNum, 0])
+
+        while i < len(prices):
+
+            if prices[i] < lastNum:
+                # do calculations
+                calc = lastNum - fee
+
+                # If > 0, add new entry to q based on most recent sum
+                if calc - q[0][0] > 0:
+                    newEntry = [prices[i], q[0][1] + calc - q[0][0]]
+                    q.appendleft(newEntry)
+
+                # Check rest of q for better options if available
+                j = 1
+                while j < len(q):
+                    # If it beats previous best sum, update it
+                    q[j][1] = max(q[j][1], calc - q[j][0])
+                    j += 1
+
+            lastNum = prices[i]
+            i += 1
+
+        # Add condition for list which ends in ascending numbers
+        if len(prices) > 1 and prices[-1] >= prices[-2]:
+            # do calculations
+            calc = lastNum - fee
+
+            # If > 0, add new entry to q based on most recent sum
+            if calc - q[0][0] > 0:
+                newEntry = [0, q[0][1] + calc - q[0][0]]
+                q.appendleft(newEntry)
+
+            # Check rest of q for better options if available
+            j = 1
+            while j < len(q):
+                # If it beats previous best sum, update it
+                q[j][1] = max(q[j][1], calc - q[j][0])
+                j += 1
+
+        largest = 0
+        for item in q:
+            largest = max(largest, item[1])
+
+        return largest
+
--- Solutions/BuySellStockWithFee/test.py
+++ Solutions/BuySellStockWithFee/test.py
@@ -24,8 +24,17 @@ class test(unittest.TestCase):
         self.assertEqual(self.a.maxProfit([9,8,7,6,5,4,3,2,1], 0), 0)
         self.assertEqual(self.a.maxProfit([1,9], 3), 5)
         self.assertEqual(self.a.maxProfit([1, 9, 13, 90, 32, 5, 16, 88, 153], 152), 0)
-        self.assertEqual(self.a.maxProfit([1, 3, 2, 9, 1, 4, 4, 1, 6], 3), 8)
+        self.assertEqual(self.a.maxProfit([1, 3, 2, 9, 1, 4, 4, 1, 6], 3), 7)
 
     def testMulti(self):
         self.assertEqual(self.a.maxProfit([2,9,5,8,16,5,17], 2), 24)
+        self.assertEqual(self.a.maxProfit([1,5,2,6,5,10], 3), 6)
+
+        # Troubleshoot this later
+        self.assertEqual(self.a.maxProfit([4,5,2,4,3,3,1,2,5,4], 1), 4)
+    
+    def testDefault(self):
+        self.assertEqual(self.a.maxProfit([1,3,7,5,10,3], 3), 6)
+        self.assertEqual(self.a.maxProfit([1,3,2,8,4,9], 2), 8)
+        
 
